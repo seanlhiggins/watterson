ea874776fc6704d4f1211062f5ced922344e2327
Sean Higgins <shiggins@looker.com>
2019-11-06 16:24:24 +0000

    added itertools

--- peeker.py
+++ peeker.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import json
 import sys
+import itertools
+
 sys.path.insert(1, '../looker_sdk')
 
 from looker_sdk import client, models
@@ -15,7 +18,17 @@ for i in range(0,data.shape[0]):
     print(data['Name'][i],data['Office'][i])
 
 all_groups = sdk.all_groups()
+all_group_names = []
 i=0
 while i < len(all_groups):
-	print(all_groups[i].name)
+	all_group_names.append(all_groups[i].name)
 	i+=1
+
+
+
+uniquemarkets = (data['Market'].unique())
+for market in uniquemarkets:
+	payload = {"name":market}
+	payloadjson=json.dumps(payload)
+	sdk.create_group(payloadjson)
+
