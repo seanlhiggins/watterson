ee6a69245e5d489fa4ef3d513360716e56f9009a
Sean Higgins <shiggins@looker.com>
2019-11-06 16:03:03 +0000

    updating with Looker SDK stuff

--- peeker.py
+++ peeker.py
@@ -1,4 +1,21 @@
-import pandas as pd 
+import pandas as pd
+import sys
+sys.path.insert(1, '../looker_sdk')
+
+from looker_sdk import client, models
+
+sdk = client.setup('looker.ini')
+
 data = pd.read_csv("examplelookerusers.csv") 
-# Preview the first 5 lines of the loaded data 
 data.head()
+# Preview the first 5 lines of the loaded data 
+
+mailgroup = {}
+for i in range(0,data.shape[0]):
+    print(data['Name'][i],data['Office'][i])
+
+all_groups = sdk.all_groups()
+i=0
+while i < len(all_groups):
+	print(all_groups[i].name)
+	i+=1
