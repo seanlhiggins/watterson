574ed8e6a23c1987ae7a145de5fc6a8942d43c56
Sean Higgins <shiggins@looker.com>
2019-11-06 22:37:00 +0000

    cleanup user function

--- peeker.py
+++ peeker.py
@@ -33,34 +33,34 @@ def get_group_id_for_group_name(group_name):
 	all_group_names_and_ids = {}
 	all_groups = sdk.all_groups()
 	i=0
+	
 	while i < len(all_groups):
 		all_group_names_and_ids[all_groups[i].name] = all_groups[i].id
 		i+=1
 	newdict = dict()
+	
 	for (k,v) in all_group_names_and_ids.items():
 		if k == group_name:
 			newdict[k] = v
 	return (newdict)
 
-
 # create_groups(csvheadername)
 
 def add_users_to_groups():
 	users = {}
-
+	users_group = dict()
+	
 	for i in range(0,data.shape[0]):
 		email = data['Email Address'][i]
 		office = data['Office'][i]
 		users[email]=office
 
 	for k,v in users.items():
-		users_group = newDict()
-		userid = sdk.user_for_credential('Email Address', x).id
-		users_group = get_group_id_for_group_name(v)
-		users_group[users_group] = userid
+		userid = sdk.user_for_credential('email', 'Kirsten.Mitchell@dyson.com')
+		groupnameid = get_group_id_for_group_name(v)
+		users_group[userid.id] = groupnameid[v]
+		print(users_group)
 		# add a function that gets all the group_ids for all the groups in the csv
 
 add_users_to_groups()
-
-
 
